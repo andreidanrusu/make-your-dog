c054675fd87bd0fe01dd78f569bb726b7204af6a
Rusu <a.rusu-1@student.utwente.nl>
2023-07-27 22:01:09 +0200

    Added logo, bootstrap grid, drawing canvas

--- make-your-dog/src/app/Container/container.component.ts
+++ make-your-dog/src/app/Container/container.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
     selector: 'app-container',
@@ -6,6 +6,21 @@ import { Component } from '@angular/core';
     styleUrls: ['./container.component.css']
 })
 
-export class ContainerComponent{
+export class ContainerComponent implements OnInit{
+    @ViewChild('canvas',{static:true}) drawingCanvas!: ElementRef;
+    canvasActive : boolean = false;
+    canvasContext : CanvasRenderingContext2D | undefined;
+    ngOnInit(): void {
+        const canvas : HTMLCanvasElement = this.drawingCanvas.nativeElement;
+        const context = canvas.getContext('2d');
+        if (context) {
+            this.canvasActive = true;
+            this.drawRect(context);
+        }
+    }
 
+    drawRect(context:CanvasRenderingContext2D){
+        context.fillRect(0,0,100,100);
+    }
+   
 }
